73dd384fe015ab4831d77ebd6615cf6cb4aa595d
cgunsbury <58825313+cgunsbury@users.noreply.github.com>
2020-05-07 15:38:10 -0500

    removed unused code

--- RobotArm_matplotlib.py
+++ RobotArm_matplotlib.py
@@ -128,20 +128,10 @@ class drawPlatforms:
         self.width = w
 
     def plotObj(self):
-        # pat.Rectangle((self.xbottom-self.width/2,self.ybottom),self.width,self.ytop)
-        # plt.plot([self.x - self.width/2, self.x - self.width/2],[self.ytop, self.ybottom], 'b')
-        # plt.plot([self.x + self.width/2, self.x + self.width/2],[self.ytop, self.ybottom], 'b')
-        # plt.plot([self.x + self.width/2, self.x - self.width/2],[self.ytop, self.ytop], 'b')
-        # plt.plot([self.x + self.width/2, self.x - self.width/2],[self.ybottom, self.ybottom], 'b')
         rect = pat.Rectangle((self.x - self.width/2,0),self.width,self.ytop,facecolor='b')
         ax.add_patch(rect)
 
     def plotGoal(self):
-        # pat.Rectangle((self.xbottom-self.width/2,self.ybottom),self.width,self.ytop)
-        # plt.plot([self.x - self.width/2, self.x - self.width/2],[self.ytop, self.ybottom], 'g')
-        # plt.plot([self.x + self.width/2, self.x + self.width/2],[self.ytop, self.ybottom], 'g')
-        # plt.plot([self.x + self.width/2, self.x - self.width/2],[self.ytop, self.ytop], 'g')
-        # plt.plot([self.x + self.width/2, self.x - self.width/2],[self.ybottom, self.ybottom], 'g')
         rect = pat.Rectangle((self.x - self.width/2,0),self.width,self.ytop,facecolor='g')
         ax.add_patch(rect)
 
